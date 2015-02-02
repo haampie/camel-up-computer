2e1b6a610d662c1cbde57f631d78ec27cd5e9506
Harmen <harmenstoppels@gmail.com>
2015-02-02 23:27:55 +0100

    Removed d file

--- main.d
+++
@@ -1,7 +0,0 @@
-main.o: main.cc main.h Game/Game.h Game/../Utilities/definitions.h \
- Game/../Camels/Camel.h Game/../Cards/Card.h \
- Game/../HashTable/HashedPosition.h \
- Game/../HashTable/../Utilities/definitions.h \
- Game/../HashTable/HashTable.h Game/../HashTable/HashedPosition.h \
- Game/../HashTable/../Camels/Camel.h Game/../Players/Player.h \
- Game/../Players/../Cards/Card.h
